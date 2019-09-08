6305f6aafd4071f01ddcdc8a72d4d532f9447e7c
osogrizz <lhtor@gmail.com>
2019-09-07 22:37:39 -0700

    Adds drag event, 10 event listeners, MVP.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 // Your code goes here
+
+// #1
 let logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseover', (event) => {
   logo.style.color = 'red';
   logo.style.transition = 'color 0.3s';
 })
 
+// #2
 logo.addEventListener('mouseleave', event => {
   logo.style.color = '#222'
   logo.style.transition = 'color 1.7s';
 })
 
+// #3
 let navItems = document.querySelectorAll('.nav-link');
 console.log(navItems);
 let arr = Array.from(navItems);
@@ -19,32 +23,37 @@ arr.map( item => {
   })
 })
 
+// #4
 let html = document.querySelector('html');
 html.addEventListener('wheel', (event) => {
   event.target.style.background = '#222';
   event.target.style.color = '#fff';
 })
 
+// #5
 html.addEventListener('dblclick', (e) => {
   e.target.style.background = '#fff';
   e.target.style.color = '#222';
 })
 
+// #6
 logo.addEventListener('keydown', (e) => {
   e.target.style.color = '#222';
 })
 
+// #7
 window.addEventListener('load', (e) => {
   alert("Hello World!")
 })
 
+// #8
 arr.map( button => {
   button.addEventListener('focus', (e) => {
     e.target.remove()
   })
 })
 
-
+// #9
 let title = document.getElementsByTagName('h2');
 let titles = Array.from(title)
 window.addEventListener('resize', (e) => {
@@ -53,11 +62,11 @@ window.addEventListener('resize', (e) => {
   })
 })
 
+// #10
 let destText = document.querySelectorAll('.destination p');
 console.log(destText)
 destText.forEach( dest => {
-
-  dest.addEventListener('select', event => {
-    console.log(event);
+  dest.addEventListener('drag', event => {
+    event.target.remove(dest);
   })
 })
